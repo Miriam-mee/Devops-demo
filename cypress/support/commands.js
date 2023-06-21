6f2e27969fd3392805a179c00a689c09577c956f
Miriam-mee <sundaymiriam93@yahoo.com>
2023-06-21 08:18:46 +0100

    added steps for locked out user

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,8 +1,8 @@
-import { LoginPage } from './pages';
+import { LoginPage, LogoutPage } from './pages';
 
 const loginPage = new LoginPage();
 
-Cypress.Commands.add('uiLogin', (username, password) => {
+Cypress.Commands.add('Login', (username, password) => {
     cy.get(loginPage.usernameInput).should('be.visible').clear().type(username)
       .get(loginPage.passwordInput).should('be.visible').clear().type(password)
       .get(loginPage.loginButton).should('be.visible').click()
